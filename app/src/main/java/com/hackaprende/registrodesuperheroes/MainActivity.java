5c1607af92a65503bfe7a2806dc1797ef702f302
Jesus Almaral <almaral17@gmail.com>
2020-11-30 12:16:08 -0600

    Open DetailActivity with explicit intent

--- app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.java
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.java
@@ -1,14 +1,27 @@
 package com.hackaprende.registrodesuperheroes;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.hackaprende.registrodesuperheroes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.saveButton.setOnClickListener(v -> {
+            openDetailActivity();
+        });
+    }
+
+    private void openDetailActivity() {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
