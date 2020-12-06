5dc8511a6bc59799df36a2a74872ce1fc6691e1b
Jesus Almaral <almaral17@gmail.com>
2020-12-06 10:43:43 -0600

    Take and pass photo to detailActivity

--- app/src/main/java/com/hackaprende/registrodesuperheroes/DetailActivity.java
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.hackaprende.registrodesuperheroes;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import com.hackaprende.registrodesuperheroes.databinding.ActivityDetailBinding;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String SUPERHERO_KEY = "superhero";
+    public static final String BITMAP_KEY = "bitmap";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,11 @@ public class DetailActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         Superhero superhero = extras.getParcelable(SUPERHERO_KEY);
+        Bitmap bitmap = extras.getParcelable(BITMAP_KEY);
+
+        if (bitmap != null) {
+            binding.imageView5.setImageBitmap(bitmap);
+        }
         binding.setSuperhero(superhero);
     }
 }
--- app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.java
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.java
@@ -1,18 +1,27 @@
 package com.hackaprende.registrodesuperheroes;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hackaprende.registrodesuperheroes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int PHOTO_REQUEST_CODE = 1000;
+    private ActivityMainBinding binding;
+    private Bitmap takenPhotoBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.saveButton.setOnClickListener(v -> {
@@ -22,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
             float rating = binding.powerBar.getRating();
             openDetailActivity(superheroName, alterEgo, bio, rating);
         });
+
+        binding.heroImage.setOnClickListener(v -> {
+            openCamera();
+        });
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, PHOTO_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PHOTO_REQUEST_CODE) {
+            if (data != null) {
+                takenPhotoBitmap = data.getExtras().getParcelable("data");
+                binding.heroImage.setImageBitmap(takenPhotoBitmap);
+            } else {
+                Toast.makeText(this, "Error taking photo", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void openDetailActivity(String superheroName, String alterEgo,
@@ -30,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.SUPERHERO_KEY, superhero);
+        intent.putExtra(DetailActivity.BITMAP_KEY, takenPhotoBitmap);
         startActivity(intent);
     }
 }
